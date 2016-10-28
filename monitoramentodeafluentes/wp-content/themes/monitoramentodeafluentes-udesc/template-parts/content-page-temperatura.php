69463dbf96e6e815b79a6809acdd956ac31c0f59
Raul Victor Rosa <raulvictorrosa@gmail.com>
2016-10-28 16:17:06 -0200

    page temperatura

---
+++ monitoramentodeafluentes/wp-content/themes/monitoramentodeafluentes-udesc/template-parts/content-page-temperatura.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package monitoramentodeafluentes-theme
+ */
+
+?>
+<div class="container">
+  <div class="row">
+		<div class="col-sm-12">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<h1 class="entry-title" style="text-align: center;">
+						<?php the_title(); ?>
+					</h1>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<div id="chartTemperatura" style="height: 400px;">
+					<?php
+						the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'monitoramentodeafluentes-udesc' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+
+				<?php if ( get_edit_post_link() ) : ?>
+					<footer class="entry-footer">
+						<?php
+							edit_post_link(
+								sprintf(
+									/* translators: %s: Name of current post */
+									esc_html__( 'Edit %s', 'monitoramentodeafluentes-udesc' ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								),
+								'<span class="edit-link">',
+								'</span>'
+							);
+						?>
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+			</article><!-- #post-## -->	
+		</div>
+	</div>
+</div><!-- /.container -->
