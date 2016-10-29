652ff34cb56bad20b90b772f8af91ddfd8ac5e74
Raul Victor Rosa <raulvictorrosa@gmail.com>
2016-10-29 00:54:29 -0200

    renamed the archives from the edited lines

--- monitoramentodeafluentes/wp-content/themes/monitoramentodeafluentes-udesc/canvasjs/profundidade.php
+++ monitoramentodeafluentes/wp-content/themes/monitoramentodeafluentes-udesc/canvasjs/profundidade.php
@@ -1,19 +1,15 @@
 <?php
-include 'select-data-prof.php';
+include 'prof-select-data.php';
 
 $dataPoints = array();
 if (is_array($data) || is_object($data)) {
   foreach($data as $row) {
-    // echo '<pre>';
     $epoch = strtotime($row['data'].' '.$row['hora']);
     $profundidade = (2.14-$row['dados']);
     $newData = array('x'=>$epoch, 'y'=>$profundidade);
     $dataPoints[] = $newData;
-    // echo '</pre>';
   }
 }
 
-// echo '<pre style="margin-top: 450px">';
 // var_dump($dataPoints);
-// echo '</pre>';
 // echo json_encode($dataPoints, JSON_NUMERIC_CHECK);
