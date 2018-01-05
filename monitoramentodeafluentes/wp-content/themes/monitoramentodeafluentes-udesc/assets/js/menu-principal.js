29d8bf34e9207fb043fccab3ff20022f6574a62a
Raul Victor Rosa <raulvictorrosa@gmail.com>
2018-01-05 19:58:11 -0200

    feat: Delete zips that already exists as archives and files

--- monitoramentodeafluentes/wp-content/themes/monitoramentodeafluentes-udesc/assets/js/menu-principal.js
+++
@@ -1,4 +0,0 @@
-$(".cabecalho .primary-menu").addClass( "collapse navbar-collapse" );
-$(".cabecalho .primary-menu .menu").addClass( "nav navbar-nav" );
-$(".cabecalho .primary-menu .menu .current-menu-item").addClass( "active" );
-$(".cabecalho .primary-menu .menu .current_page_item").addClass( "active" );
